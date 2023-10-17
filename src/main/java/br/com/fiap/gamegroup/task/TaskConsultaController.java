16cd492f450ae4c93e88b588ce8b5a1a347e1dfe
matlencar <rm94642@fiap.com.br>
2023-10-16 23:25:16 -0300

    feat login social

--- src/main/java/br/com/fiap/gamegroup/task/TaskConsultaController.java
+++ src/main/java/br/com/fiap/gamegroup/task/TaskConsultaController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,6 +34,26 @@ public class TaskConsultaController {
         } else {
             redirect.addFlashAttribute("error", "Consulta não encontrada");
         }
+        
+        return "redirect:/task/";
+    }
+
+    @DeleteMapping
+    public String deleteconsulta(Consulta consulta) {
+
+        return "redirect:/task/";
+    }
+
+    @GetMapping("form")
+    public String form(){
+
+        return "task/form";
+    }
+
+    @PostMapping
+    public String cadastrar(RedirectAttributes redirect) {
+
+        redirect.addFlashAttribute("sucess", "Medico cadastrado com sucesso com sucesso");
         
         return "redirect:/task/";
     }
